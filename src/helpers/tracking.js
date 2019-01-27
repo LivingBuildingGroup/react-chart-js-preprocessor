fc98adac851867c5802ae3cb85802d0eae3a3203
bradcgarner <brad@bradgarner.com>
2019-01-27 17:38:51 -0500

    bug fix in createGoogleTagManagerClaass

--- src/helpers/tracking.js
+++ src/helpers/tracking.js
@@ -3,9 +3,10 @@ const { isObjectLiteral } = require('conjunction-junction');
 export const createGoogleTagManagerClass = (state, action) => {
   const _action = action ? action : state.graphName ;
   const preSetName = state.preSets && state.preSets[state.preSetIdActive] && state.preSets[state.preSetIdActive].name ? state.preSets[state.preSetIdActive].name : 'no-pre-set';
-  const preSetIdClass = preSetName.join('-');
+  const preSetArr = preSetName.split(' ');
+  const preSetIdClass = preSetArr.join('-');
   const listOfEventsObj = isObjectLiteral(state.titleText) ? state.titleText : {} ;
-  let listOfEventsArr = [];
+  const listOfEventsArr = [];
   for(let id in listOfEventsObj){
     listOfEventstate.push[id];
   }
