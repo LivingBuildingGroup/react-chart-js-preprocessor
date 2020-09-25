b10b2972b895252f6bd92bfd539619bb3a515f79
bradcgarner <brad@bradgarner.com>
2020-09-25 11:50:00 -0400

    finished controls refactoring

--- src/control-helpers.js
+++ src/control-helpers.js
@@ -1,15 +1,6 @@
 import { isObjectLiteral } from 'conjunction-junction';
 
-export const formatControlsWithoutPreSets = (state, that) => {
-
-  const controlNamesTop = [];
-  const controlIconNamesTop = [];
-  const controlFuncsTop = [];
-  const controlLabelsTop= [];
-  const controlNamesBot = [];
-  const controlIconNamesBot = [];
-  const controlFuncsBot = [];
-  const controlLabelsBot= [];
+export const formatControlsTopAndBot = (state, that) => {
 
   const controlsTop = [];
   const controlsBot = [];
@@ -22,10 +13,6 @@ export const formatControlsWithoutPreSets = (state, that) => {
       func:     that.printGraph,
       label:    'Print the graph on letter size landscape (allow a few seconds for the graph to render before print preview starts).',
     });
-    controlNamesTop.push('print');
-    controlIconNamesTop.push('Print');
-    controlFuncsTop.push(that.printGraph);
-    controlLabelsTop.push('Print the graph on letter size landscape (allow a few seconds for the graph to render before print preview starts).');
   }
   if(state.backgroundAllow){
     controlsTop.push({
@@ -35,10 +22,6 @@ export const formatControlsWithoutPreSets = (state, that) => {
       func:     that.handleBackgroundColor,
       label:    'Toggle between white and dark gray graph background.',
     });
-    controlNamesTop.push('background');
-    controlIconNamesTop.push('PaletteSolid');
-    controlFuncsTop.push(that.handleBackgroundColor);
-    controlLabelsTop.push('Toggle white graph background');
   }
   if(state.yAxisAllow){
     controlsTop.push({
@@ -48,10 +31,6 @@ export const formatControlsWithoutPreSets = (state, that) => {
       func:     that.handleYAxisSelector,
       label:    'Toggle Y-Axis settings',
     });
-    controlNamesTop.push('y-Axis');
-    controlIconNamesTop.push('ArrowsAltV');
-    controlFuncsTop.push(that.handleYAxisSelector);
-    controlLabelsTop.push('Toggle Y-Axis settings');
   }
   if(state.selectorsAllow){
     controlsBot.push({
@@ -61,124 +40,45 @@ export const formatControlsWithoutPreSets = (state, that) => {
       func:     that.toggleSelectorsPopover,
       label:    'Open graph customization options',
     });
-    controlNamesBot.push('selector');
-    controlIconNamesBot.push('Edit');
-    controlFuncsBot.push(that.toggleSelectorsPopover);
-    controlLabelsBot.push('Open graph customization options');
   }
   return {
     controlsTop,
     controlsBot,
-    controlNamesTop,
-    controlIconNamesTop,
-    controlFuncsTop,
-    controlLabelsTop,
-    controlNamesBot,
-    controlIconNamesBot,
-    controlFuncsBot,
-    controlLabelsBot,
   };
 };
 
-export const formatPreSetsForControls = (preSets, icons={}, that) => {
+export const formatControlsPresets = (preSets, that) => {
   const controlsPresets = [];
   if(!isObjectLiteral(preSets)) {
-    return {controlsPresets};
-    return { 
-      preSetIds  : [],
-      preSetNames: [],
-      preSetIconNames: [],
-      preSetFuncs: [],
-    };
+    return controlsPresets;
   }
-  const preSetIds = [];
   for(let id in preSets){
     const thisPreset = preSets[id];
     controlsPresets.push({
       name:     thisPreset.name || 'pre-set',
-      id:       'print',
+      id,
       iconName: thisPreset.icon || 'CoffeePot',
       func:     ()=>that.handlePreSetSelect(id),
       label:    thisPreset.name || 'pre-set',
-  
-    })
-    preSetIds.push(id);
+    });
   }
-  preSetIds.sort();
-  const preSetNames = preSetIds.map(id=>{
-    return preSets[id].name || 'pre-set';
+  controlsPresets.sort((a,b)=>{
+    if(a.id > b.id){
+      return 1;
+    }
+    return -1;
   });
-  const preSetIconNames = preSetIds.map(id=>preSets[id].icon);
-    
-  const preSetFuncs = preSetIds.map(id=>{
-    return ()=>that.handlePreSetSelect(id);
-  });
-  return {
-    preSetIds,
-    preSetNames,
-    preSetIconNames,
-    preSetFuncs,
-    controlsPresets,
-  };
+
+  return controlsPresets;
 };
 
 export const formatControls = (state, that) => {
   const {
     controlsTop,
     controlsBot,
-    controlNamesTop,
-    controlIconNamesTop,
-    controlFuncsTop,
-    controlLabelsTop,
-    controlNamesBot,
-    controlIconNamesBot,
-    controlFuncsBot,
-    controlLabelsBot,
-  } = formatControlsWithoutPreSets(state, that);
+  } = formatControlsTopAndBot(state, that);
   
-  const {
-    controlsPresets,
-    preSetIds,
-    preSetNames,
-    preSetIconNames,
-    preSetFuncs,
-  } = formatPreSetsForControls(state.preSets, state.icons, that);
-
-  const controlNames = [
-    ...controlNamesTop, 
-    ...preSetNames,
-    ...controlNamesBot, 
-  ];
-  const controlIds = [
-    ...controlNamesTop, 
-    ...preSetIds,
-    ...controlNamesBot, 
-  ];
-  const controlIconNames = [
-    ...controlIconNamesTop, 
-    ...preSetIconNames,
-    ...controlIconNamesBot, 
-  ];
-  const controlFuncs = [
-    ...controlFuncsTop, 
-    ...preSetFuncs,
-    ...controlFuncsBot, 
-  ];
-  const controlLabels = [
-    ...controlLabelsTop, 
-    ...preSetNames,
-    ...controlLabelsBot, 
-  ];
-
-  const controlsOld = controlNames.map((n,i)=>{
-    return {
-      name: n,
-      id:       controlIds[i],
-      iconName: controlIconNames[i],
-      func:     controlFuncs[i],
-      label:    controlLabels[i],
-    };
-  });
+  const controlsPresets = formatControlsPresets(state.preSets, that);
 
   const controls = [
     ...controlsTop,
